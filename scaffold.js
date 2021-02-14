a1e09dac43815b372e16c8cc01ae91250697d78c
Borche <hello@borche.dev>
2021-02-14 20:45:54 +0100

    Upate package.

--- scaffold.js
+++ scaffold.js
@@ -4,66 +4,60 @@ let hexo = null;
 let SCAFFOLD_DIR = null;
 
 function setup(hexoInstance) {
-    hexo = hexoInstance;
-    SCAFFOLD_DIR = hexo.scaffold_dir;
+  hexo = hexoInstance;
+  SCAFFOLD_DIR = hexo.scaffold_dir;
 }
 
 function getAllScaffoldNames() {
-    let allScaffolds = [];
-    const files = fs.readdirSync(SCAFFOLD_DIR, {withFileTypes: true});
-    files.forEach(file => {
-        if (file.isFile()) {
-            allScaffolds.push(
-                path.basename(file.name, ".md")
-            );
-        }
-    })
-    return allScaffolds;
+  let allScaffolds = [];
+  const files = fs.readdirSync(SCAFFOLD_DIR, { withFileTypes: true });
+  files.forEach((file) => {
+    if (file.isFile()) {
+      allScaffolds.push(path.basename(file.name, ".md"));
+    }
+  });
+  return allScaffolds;
 }
 
 function update(name, content) {
-    return fs.writeFileSync(path.join(SCAFFOLD_DIR, name + ".md"), content);
+  return fs.writeFileSync(path.join(SCAFFOLD_DIR, name + ".md"), content);
 }
 
 function getAllScaffoldData() {
-    const allScaffoldNames = getAllScaffoldNames();
-    let allScaffolds = [];
-    allScaffoldNames.forEach(
-        scaffoldName => {
-            let fileName = path.join(SCAFFOLD_DIR, scaffoldName + ".md");
-            allScaffolds.push(
-                {
-                    name: scaffoldName,
-                    fileName: fileName,
-                    content: fs.readFileSync(fileName)
-                }
-            )
-        }
-    )
-    return allScaffolds;
+  const allScaffoldNames = getAllScaffoldNames();
+  let allScaffolds = [];
+  allScaffoldNames.forEach((scaffoldName) => {
+    let fileName = path.join(SCAFFOLD_DIR, scaffoldName + ".md");
+    allScaffolds.push({
+      name: scaffoldName,
+      fileName: fileName,
+      content: fs.readFileSync(fileName),
+    });
+  });
+  return allScaffolds;
 }
 
 function rename(oldName, newName) {
-    const oldPath = path.join(SCAFFOLD_DIR, oldName + ".md");
-    const newPath = path.join(SCAFFOLD_DIR, newName + ".md");
-    return fs.renameSync(oldPath, newPath);
+  const oldPath = path.join(SCAFFOLD_DIR, oldName + ".md");
+  const newPath = path.join(SCAFFOLD_DIR, newName + ".md");
+  return fs.renameSync(oldPath, newPath);
 }
 
 function create(name) {
-    return update(name, "---\ntitle: {{ title }}\ntags:\n---");
+  return update(name, "---\ntitle: {{ title }}\ntags:\n---");
 }
 
 function deleteScaffold(name) {
-    const scaffoldPath = path.join(SCAFFOLD_DIR, name + ".md");
-    return fs.unlinkSync(scaffoldPath);
+  const scaffoldPath = path.join(SCAFFOLD_DIR, name + ".md");
+  return fs.unlinkSync(scaffoldPath);
 }
 
 module.exports = {
-    setup: setup,
-    getNames: getAllScaffoldNames,
-    getAllData: getAllScaffoldData,
-    update: update,
-    rename: rename,
-    create: create,
-    delete: deleteScaffold
-}
+  setup: setup,
+  getNames: getAllScaffoldNames,
+  getAllData: getAllScaffoldData,
+  update: update,
+  rename: rename,
+  create: create,
+  delete: deleteScaffold,
+};
