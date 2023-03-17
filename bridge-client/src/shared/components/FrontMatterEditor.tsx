e1658fe099c5d1dd6ff510fe6d656869f2a303ed
Borche <hello@borche.dev>
2023-03-17 05:07:58 +0100

    Update front matter editor

--- bridge-client/src/shared/components/FrontMatterEditor.tsx
+++ bridge-client/src/shared/components/FrontMatterEditor.tsx
@@ -1,7 +1,9 @@
-import React, { useLayoutEffect, useState } from "react";
-import { Alert, Dialog, Intent } from "@blueprintjs/core";
-import Editor from "@monaco-editor/react";
+import { IAnnotation } from "react-ace";
+import { Alert, Callout, Dialog, Intent } from "@blueprintjs/core";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import useAPI from "../useAPI";
+import CodeEditor from "@uiw/react-textarea-code-editor";
+
 const frontMatterHelper = require("hexo-front-matter");
 
 type FrontMatterEditorProps = {
@@ -11,50 +13,50 @@ type FrontMatterEditorProps = {
 };
 
 export default function FrontMatterEditor({ value, onClose, isOpen }: FrontMatterEditorProps) {
-  const [metaData, setMetaData] = useState("");
+  const [metaData, setMetadata] = useState("");
+  const [errorMarker, setErrorMarker] = useState<IAnnotation>();
   const [canBeClosed, setCanBeClosed] = useState(true);
-
   useLayoutEffect(() => {
-    setMetaData(value);
+    setMetadata(value.trim());
   }, [value]);
+
+  useEffect(() => {
+    validateFrontMatter();
+    //eslint-disable-next-line
+  }, [metaData]);
 
   const { data: userPrefs } = useAPI({
     method: "GET",
     url: "settings/bridge/getAsJson",
   });
 
-  function getErrorMarkers(content: string) {
+  function validateFrontMatter(): boolean {
     try {
-      frontMatterHelper.parse(content);
-      if (!content.trim().endsWith("---")) {
-        return [
-          {
-            startLineNumber: content.split("\n").length - 1,
-            endLineNumber: content.split("\n").length - 1,
-            startColumn: 0,
-            endColumn: 0,
-            message: "Front matter must end with ---",
-            severity: 8,
-          },
-        ];
+      frontMatterHelper.parse(metaData);
+      if (!metaData.trim().endsWith("---")) {
+        setErrorMarker({
+          row: metaData.split("\n").length - 1,
+          column: 0,
+          text: "Front matter must end with ---",
+          type: "error",
+        });
+        return false;
       }
-      return [];
+      setErrorMarker(undefined);
+      return true;
     } catch (error: any) {
-      return [
-        {
-          startLineNumber: error.mark.line + 1,
-          endLineNumber: error.mark.line + 1,
-          startColumn: error.mark.column + 1,
-          endColumn: error.mark.column + 1,
-          message: error.message,
-          severity: 8,
-        },
-      ];
+      setErrorMarker({
+        row: error.mark.line,
+        column: 0,
+        text: error.message,
+        type: "error",
+      });
+      return false;
     }
   }
 
   function onCloseEditor() {
-    const isMetadataValid = getErrorMarkers(metaData).length === 0;
+    const isMetadataValid = validateFrontMatter();
     if (isMetadataValid) {
       setCanBeClosed(true);
       onClose(metaData);
@@ -64,52 +66,74 @@ export default function FrontMatterEditor({ value, onClose, isOpen }: FrontMatte
   }
 
   return (
-    <Dialog
-      icon="annotation"
-      title="Front matter editor"
-      isOpen={isOpen}
-      canOutsideClickClose={false}
-      onClose={onCloseEditor}
-      style={{ width: "50vw" }}
-    >
-      <Editor
-        width={"fit-parent"}
-        height={"50vh"}
-        language="yaml"
-        defaultValue={metaData}
-        options={{
-          theme: "Xcode_default",
-          minimap: { enabled: false },
-          contextmenu: false,
-          fontSize: userPrefs.editorFontSize || 14,
+    <>
+      <Dialog
+        icon="annotation"
+        title="Front matter editor"
+        isOpen={isOpen}
+        canOutsideClickClose={false}
+        onClose={onCloseEditor}
+        style={{
+          minWidth: "60%",
         }}
-        onMount={function handleEditorDidMount(editor, monaco) {
-          editor.onDidChangeModelContent((e) => {
-            const newContent = editor.getValue();
-            setMetaData(newContent);
-            const errorMarkers = getErrorMarkers(newContent);
-            monaco.editor.setModelMarkers(editor.getModel()!, "Bridge", [...errorMarkers]);
-          });
-        }}
-      />
-      <Alert
-        isOpen={!canBeClosed}
-        intent={Intent.PRIMARY}
-        icon="error"
-        confirmButtonText="Go back"
-        cancelButtonText="Discard changes"
-        onCancel={() => {
-          setMetaData(value);
-          setCanBeClosed(true);
-          onClose(value);
-        }}
-        onConfirm={() => setCanBeClosed(true)}
       >
-        <p>
-          <b>Front matter contains errors.</b>
-        </p>
-        <p>You can go back and correct these errors, or discard the changes.</p>
-      </Alert>
-    </Dialog>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(2, 1fr)",
+            gridTemplateRows: "1fr",
+            gridColumnGap: "0px",
+            gridRowGap: "0px",
+          }}
+        >
+          <CodeEditor
+            value={metaData}
+            language="yaml"
+            placeholder="Please enter frontmatter in yaml format."
+            onChange={(evn) => setMetadata(evn.target.value.trim())}
+            padding={15}
+            style={{
+              fontSize: userPrefs.editorFontSize || 14,
+              fontFamily: "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+            }}
+          />
+          <Callout
+            title={errorMarker ? "Front matter contains errors." : "Front matter is valid."}
+            intent={errorMarker ? Intent.DANGER : Intent.SUCCESS}
+            icon={errorMarker ? "code" : "endorsed"}
+            style={{
+              width: "100%",
+            }}
+          >
+            <CodeEditor
+              value={errorMarker ? errorMarker?.text : metaData}
+              disabled
+              language="yaml"
+              style={{
+                fontFamily: "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+              }}
+            />
+          </Callout>
+        </div>
+        <Alert
+          isOpen={!canBeClosed}
+          intent={Intent.PRIMARY}
+          icon="error"
+          confirmButtonText="Go back"
+          cancelButtonText="Discard changes"
+          onCancel={() => {
+            setMetadata(value);
+            setCanBeClosed(true);
+            onClose(value);
+          }}
+          onConfirm={() => setCanBeClosed(true)}
+        >
+          <p>
+            <b>Front matter contains errors.</b>
+          </p>
+          <p>You can go back and correct these errors, or discard the changes.</p>
+        </Alert>
+      </Dialog>
+    </>
   );
 }
